6074e304ab5505f9f86eb3e3dc744cf2197dc8dd
Ruslan <info@liberdesign.eu>
2020-01-01 20:19:06 +0000

    example full

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Product } from './product.model';
 
 @Injectable()
@@ -6,13 +6,50 @@ export class ProductsService {
     private products: Product[] = [];
 
     insertProduct(title: string, desc: string, price: number) {
-        const prodId = new Date().toString();
-        const newProduct = new Product( prodId, title, desc, price);
+        const prodId = Math.random().toString();
+        const newProduct = new Product(prodId, title, desc, price);
         this.products.push(newProduct);
         return prodId;
     }
 
-    getAllProducts() {
+    getProducts() {
         return [...this.products];
+    }
+
+    getSingleProduct(prodId: string) {
+        const product = this.products.find(prod => prod.id === prodId);
+        if (!product) {
+            throw new NotFoundException('Impossibiler trovare il prodotto');
+        }
+        return { ...product }
+    }
+
+    updateProduct(productId: string, title: string, desc: string, price: number) {
+        const [product, index] = this.findProduct(productId);
+        const updatedProduct = { ...product };
+        if (title) {
+            updatedProduct.title = title;
+        }
+        if (desc) {
+            updatedProduct.description = desc;
+        }
+        if (price) {
+            updatedProduct.price = price;
+        }
+        this.products[index] = updatedProduct;
+    }
+
+    deleteProduct(prodId: string) {
+        const index = this.findProduct(prodId)[1];
+        this.products.splice(index, 1);
+    }
+
+    private findProduct(id: string): [Product, number] {
+        const productIndex = this.products.findIndex(prod => prod.id === id);
+        const product = this.products[productIndex];
+        if (!product) {
+            throw new NotFoundException('Could not find product.');
+        }
+        return [product, productIndex];
     }
 }
